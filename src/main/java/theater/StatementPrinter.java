597ea493759ac566ee54c37e6ba9f1974fa223bd
Zachary Klouchnikov <zachary.klou@gmail.com>
2025-11-19 17:19:27 -0500

    Task 2.2

--- src/main/java/theater/StatementPrinter.java
+++ src/main/java/theater/StatementPrinter.java
@@ -32,7 +32,7 @@ public class StatementPrinter {
         for (Performance p : getInvoice().getPerformances()) {
 
             // add volume credits
-            volumeCredits += Math.max(p.getAudience() - Constants.BASE_VOLUME_CREDIT_THRESHOLD, 0);
+            volumeCredits += getVolumeCredits(p);
             // add extra credit for every five comedy attendees
             if ("comedy".equals(getPlay(p).getType())) {
                 volumeCredits += p.getAudience() / Constants.COMEDY_EXTRA_VOLUME_FACTOR;
@@ -46,6 +46,12 @@ public class StatementPrinter {
         result.append(String.format("Amount owed is %s%n", frmt.format(totalAmount / Constants.PERCENT_FACTOR)));
         result.append(String.format("You earned %s credits%n", volumeCredits));
         return result.toString();
+    }
+
+    private static int getVolumeCredits(Performance performance) {
+        int volumeCredits = 0;
+        volumeCredits += Math.max(performance.getAudience() - Constants.BASE_VOLUME_CREDIT_THRESHOLD, 0);
+        return volumeCredits;
     }
 
     private static Play getPlay(Performance performance) {
